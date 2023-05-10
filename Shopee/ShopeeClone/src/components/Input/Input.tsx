f2393ecb132f419cc9920a88f2e3c70f08d90b5d
Nguyen Hai <hainvhe141786@fpt.edu.vn>
2023-05-10 22:11:59 +0700

    Create ui for page product list

--- Shopee/ShopeeClone/src/components/Input/Input.tsx
+++ Shopee/ShopeeClone/src/components/Input/Input.tsx
@@ -1,27 +1,43 @@
-import React from 'react'
+import React, { InputHTMLAttributes } from 'react'
 import { RegisterOptions, UseFormRegister } from 'react-hook-form'
 
-interface Props {
-  type: React.HTMLInputTypeAttribute
+interface Props extends InputHTMLAttributes<HTMLInputElement> {
   errorMessage?: string
-  placeholder?: string
-  className?: string
-  name: string
-  register: UseFormRegister<any>
-  autoComplete?: string
+  classNameInput?: string
+  classNameError?: string
+  register?: UseFormRegister<any>
+  rules?: RegisterOptions
 }
 
-export default function Input({ type, errorMessage, name, register, className, placeholder, autoComplete }: Props) {
+export default function Input({
+  type,
+  errorMessage,
+  className,
+  placeholder,
+  autoComplete,
+  name,
+  classNameInput = 'w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm',
+  classNameError = 'mt-1 min-h-[1.25rem] text-sm text-red-600',
+  register,
+  rules
+}: Props) {
+  const registerResult = register && name ? register(name, rules) : {}
   return (
     <div className={className}>
       <input
         type={type}
-        className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
+        // className='w-full rounded-sm border border-gray-300 p-3 outline-none focus:border-gray-500 focus:shadow-sm'
+        className={classNameInput}
         placeholder={placeholder}
         autoComplete={autoComplete}
-        {...register(name)}
+        {...registerResult}
       />
-      <div className='mt-1 min-h-[1.25rem] text-sm text-red-600'>{errorMessage}</div>
+      <div
+        // className='mt-1 min-h-[1.25rem] text-sm text-red-600'
+        className={classNameError}
+      >
+        {errorMessage}
+      </div>
     </div>
   )
 }
